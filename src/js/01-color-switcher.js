38d34323160be06cee95e11bd4a974bc9a42863c
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-04-06 17:18:21 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+};
 
+refs.btnStop.disable = true;
+let colorInterval = null;
+
+refs.btnStart.addEventListener('click', () => {
+    refs.btnStart = true;
+    refs.btnStop = false;
+
+    colorInterval = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, 1000);
+});
+
+refs.btnStop.addEventListener('click', () =>{
+    clearInterval(colorInterval);
+    refs.btnStart = false;
+    refs.btnStop = true;
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  
